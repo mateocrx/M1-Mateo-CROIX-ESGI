b1d3b3e44b742120571d246893739db9a8090273
mateocrx <113882813+mateocrx@users.noreply.github.com>
2022-09-21 10:26:30 +0200

    cc

--- TP3/TP Python/loca.py
+++ TP3/TP Python/loca.py
@@ -13,36 +13,31 @@ f = open("TP PYTHON/access.log.txt",'r')
 text = f.read()
 
 
-try:
-    ips = [] 
-    regex = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b',text)
-    if regex is not None:
-        for match in regex:
-            if match not in ips:
+ips = [] 
+regex = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b',text)
+if regex is not None:
+    for match in regex:
+        if match not in ips:
 
-                current_ip = {
-                    'ip':match,
-                    'Latitude':"",
-                    'Longitude':"",
-                }
-                loca = geocoder.ip(match)
-                current_ip["Latitude"] = loca.lat
-                current_ip["Longitude"] = loca.lng
+            current_ip = {
+                'ip':match,
+                'Latitude':"",
+                'Longitude':"",
+            }
+            loca = geocoder.ip(match)
+            current_ip["Latitude"] = loca.lat
+            current_ip["Longitude"] = loca.lng
 
-                ips.append(current_ip)
+            ips.append(current_ip)
 
-    print(ips)
-
-
-    def supprimeDoublon(current_ip):
-        for i in current_ip:
-            if i not in current_ip:
-                current_ip.append(i)
-            return
+print(ips)
 
 
-except KeyboardInterrupt:
-    exit
+def supprimeDoublon(current_ip):
+    for i in current_ip:
+        if i not in current_ip:
+            current_ip.append(i)
+        return
 
 m = folium.Map(location=[45.372, -121.6972], zoom_start=12, tiles="Stamen Terrain")
 
